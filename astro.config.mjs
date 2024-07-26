49e61a454ac59fc9a9c72ae94b2a7e197ad41fdc
JorgePascualGracia2001 <43232543+JorgePascualGracia2001@users.noreply.github.com>
2024-07-26 22:38:33 +0200

    publish

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://JorgePascualGracia2001.github.io',
+  base: 'astroDemo',
+})
