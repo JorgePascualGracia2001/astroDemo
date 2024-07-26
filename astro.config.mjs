ba0fd313b8815b28ca06e4142df08fe252b80096
JorgePascualGracia2001 <43232543+JorgePascualGracia2001@users.noreply.github.com>
2024-07-26 23:12:36 +0200

    a

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import vercel from "@astrojs/vercel/serverless";
+
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://jorgepascualgracia2001.github.io',
-  base: 'astroDemo',
-})
+  output: "server",
+  adapter: vercel()
+});
